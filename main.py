865ba33c1567020d24afd9d2ac6da3e59cb96f42
Muscalagiu Anca Ioana <anca.muscalagiu@stud.ubbcluj.ro>
2021-07-16 22:14:15 +0300

    First Week Ready

---
+++ main.py
@@ -0,0 +1,7 @@
+import pyspark as py
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+text = spark.read.options(header='True').csv("C:\\Users\\LAPTOP_MIA\\PycharmProjects\\Covid\\CovidCasesWithoutDate.txt")
+text.printSchema()
+text.show(n=10, truncate=False)
