3903c2acb412090ea7405e70088f8f0f42c458fd
Muscalagiu Anca Ioana <anca.muscalagiu@stud.ubbcluj.ro>
2021-07-18 20:37:01 +0300

    w1

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import pyspark as py
 from pyspark.sql import SparkSession
 
+
+def readAndProcess(spark):
+    dataFr = spark.read.options(header='True').csv(
+        "C:\\Users\\LAPTOP_MIA\\PycharmProjects\\Covid\\CovidCasesWithoutDate.txt",
+        inferSchema=True, nullValue='')
+    dataFr = dataFr.fillna({'High School Cases': 0, 'Middle School Cases': 0, 'Elementary School Cases': 0})
+    return dataFr
+
+
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
-text = spark.read.options(header='True').csv("C:\\Users\\LAPTOP_MIA\\PycharmProjects\\Covid\\CovidCasesWithoutDate.txt",inferSchema=True)
-text.printSchema()
-text.show(n=10, truncate=False)
+dataFrame = readAndProcess(spark)
+dataFrame.printSchema()
+dataFrame.show(n=40, truncate=False)
